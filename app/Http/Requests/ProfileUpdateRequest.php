2c4b8a1cb53aff038ea514292f1e3ed5bbce4ff0
Zeljun P. Antipuesto <zpantipuesto00524@usep.edu.ph>
2024-12-07 22:24:43 +0800

    12/07/2024
    Refresh Back end, Models, Controllers For User

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,8 +16,9 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name'  => ['required', 'string', 'max:255'],
+            'email'      => [
                 'required',
                 'string',
                 'lowercase',
