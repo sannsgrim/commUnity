ff0dd5dfa9ad64d7293f6daa8222f4693e4f769f
Kurt <kbbferrer00019@usep.edu.ph>
2025-01-04 15:35:02 +0800

    columns final

--- resources/js/Components/AccountsPanel.vue
+++ resources/js/Components/AccountsPanel.vue
@@ -57,7 +57,7 @@ const roles = ref([
                 <Column field="username" header="Userame" class="text-sm"></Column>
                 <Column field="email" header="Email" class="text-sm"></Column>
                 <Column field="role" header="Role" class="text-sm"></Column>
-                <Column field="quantity" header="Quantity" class="text-sm">
+                <Column field="actions" header="Actions" class="text-sm">
                     <template #body="{ data }">
                         <Button
                             icon="pi pi-search"
@@ -77,7 +77,7 @@ const roles = ref([
 
 <style>
 /* Backdrop blur for the modal */
-.dialog-with-blur .p-dialog-mask {
+.dialog-with-blur{
     backdrop-filter: blur(8px); /* Apply blur effect */
     background: rgba(0, 0, 0, 0.5); /* Semi-transparent overlay */
 }
